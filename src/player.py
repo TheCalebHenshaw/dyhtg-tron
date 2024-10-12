98afe55386b7486c309d35270d15266e0f83c1ad
Caleb Henshaw <145933440+TheCalebHenshaw@users.noreply.github.com>
2024-10-12 12:36:46 +0100

    player

--- src/player.py
+++ src/player.py
@@ -3,9 +3,10 @@ from color.py import Color
 
 
 class Player:
-    def __init__(self,x,y,color,controls) -> None:
+    def __init__(self,x,y,color,controls,name) -> None:
         self.x = x
         self.y = y
+        self.name = name
         self.color = Color.BLUE
         self.controls = controls
         self.trail = [(self.x,self.y)]
@@ -36,4 +37,26 @@ class Player:
             self.y-=self.speed
     
         self.trail.append((self.x,self.y))
-    
+
+    def draw(self, screen):
+        #draw player as rectangle for now
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.size, self.size))
+
+        # player trail
+        for segment in self.trail:
+            pygame.draw.rect(screen, self.color, (segment[0], segment[1], self.size, self.size))
+
+
+    def checkForCollision(self,arenaWidth,arenaHeight):
+        #checks if they have collided into the wall
+        if self.x >= arenaWidth or self.x < 0 or self.y >= arenaHeight or self.y < 0:
+            self.alive = False
+        # check if player self collides
+
+        if (self.x,self.y) in self.trail[:-1]:
+            self.alive = False
+    def reset(self,x,y):
+        self.x = x
+        self.y = y
+        self.trail = [(self.x,self.y)]
+        self.direction = "RIGHT"
