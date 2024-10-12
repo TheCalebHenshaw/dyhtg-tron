260be6ff39f528c32de62e6019c40a188eb0c995
Abhishek Shadakshari <abhishadakshari@icloud.com>
2024-10-12 12:36:36 +0100

    starting main

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from player import Player
+
+def playerStart() -> None:
+
+    #opens when start game button on home_screen is clicked
+    #function to allow players to choose their name and colour
+
+    
+
+
+
